5b7f25cb71276680765d17feb7d9b2598611de34
Pavel Karpenko <ospkiev@gmail.com>
2019-03-10 00:11:45 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main'
 import Footer from './Footer/Footer';
 
-
-
 class App extends Component {
+
+  state = {
+    sum: 2,
+  }
+
+  incrementFunc = () => {
+    this.setState(prev => ({
+      sum: prev.sum + 1,
+    }))
+  }
+
+  decrementFunc = () => {
+    if (this.state.sum === 0) {
+      return;
+    }
+    this.setState(prev => ({
+      sum: prev.sum - 1,
+    }))
+  }
+
   render() {
+    const { sum } = this.state;
     return (
       <div className="App">
         <Header />
-        <Main />
+        <Main sum={sum} />
         <Footer />
+        <p className={styles.counter}>{sum}</p>
+        <button onClick={this.incrementFunc}>PLUS</button>
+        <button onClick={this.decrementFunc}>MINUS</button>
+
       </div>
     );
   }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -15,53 +15,53 @@ import stars from '../img/order/product/stars.svg';
 import start_chat from '../img/didYouFind/start_chat.svg';
 import arrow_left_icon from '../img/order/arrow_left_icon.svg'
 
-const Main = () => {
+const Main = ({ sum }) => {
     return (
         <div className={styles.main}>
-            <img src={arrow_left_icon} className={styles.arrow_left_icon} />
-            <h2 className={styles.text}>2 Продукти у кошику</h2>
+            <img src={arrow_left_icon} className={styles.arrow_left_icon} alt='img' />
+            <h2 className={styles.text}><span>{sum}</span>Продукти у кошику</h2>
             <div className={styles.order_wrapper}></div>
-            <img src={service_icon} className={styles.service_icon} />
+            <img src={service_icon} className={styles.service_icon} alt='img' />
             <p className={styles.service_text}>Service</p>
             <p className={styles.instalation}>Installation</p>
             <p className={styles.instalation_service}>Installation service is one of our all-ti...<br />
                 <span className={styles.learn_more}>Learn More</span> </p>
             <p className={styles.price}>$19</p>
             <p className={styles.delete_container}></p>
-            <img src={delete_icon_right} className={styles.delete_icon_right} />
-            <img src={delete_icon_left} className={styles.delete_icon_left} />
-            <img src={order} className={styles.order_img} alt='image' />
+            <img src={delete_icon_right} className={styles.delete_icon_right} alt='img' />
+            <img src={delete_icon_left} className={styles.delete_icon_left} alt='img' />
+            <img src={order} className={styles.order_img} alt='img' />
 
             <div>
                 <p className={styles.total}>Сума:</p>
                 <p className={styles.sum}>$119.70</p>
-                <img src={checkout_bg} className={styles.checkout_bg} alt='image' />
-                <img src={benefits} className={styles.benefits} alt='image' />
+                <img src={checkout_bg} className={styles.checkout_bg} alt='img' />
+                <img src={benefits} className={styles.benefits} alt='img' />
             </div>
             <div>
                 <p className={styles.button_text}>Перейти на чекаут</p>
-                <img src={button_bg} className={styles.button_bg} alt='image' />
-                <img src={stroke_icon} className={styles.stroke_icon} />
+                <img src={button_bg} className={styles.button_bg} alt='img' />
+                <img src={stroke_icon} className={styles.stroke_icon} alt='img' />
             </div>
             <div className={styles.product_wrapper}></div>
-            <img src={product} className={styles.product_img} />
-            <img src={html5} className={styles.html5} />
+            <img src={product} className={styles.product_img} alt='img' />
+            <img src={html5} className={styles.html5} alt='img' />
             <p className={styles.gradient}></p>
             <p className={styles.travelino_agency}>Travelino Agency Te</p>
             <span className={styles.old_price}>$1,800</span>
             <span className={styles.new_price}>$1,555</span>
-            <img src={stars} className={styles.stars} />
+            <img src={stars} className={styles.stars} alt='img' />
             <p className={styles.delete_product_container}></p>
             <p className={styles.product_text_bottom}> I want to use it <span className={styles.only_once}>only once</span> </p>
-            <img src={delete_icon_right} className={styles.delete_product_icon_right} />
-            <img src={delete_icon_left} className={styles.delete_product_icon_left} />
+            <img src={delete_icon_right} className={styles.delete_product_icon_right} alt='img' />
+            <img src={delete_icon_left} className={styles.delete_product_icon_left} alt='img' />
 
             <div className={styles.adviser_wrapper}></div>
-            <img src={adviser} className={styles.adviser} alt='image' />
+            <img src={adviser} className={styles.adviser} alt='img' />
             <p className={styles.adviser_text}>шукаєте щось особливе?
                Підберіть бажане разом з консультантом!
             </p>
-            <img src={start_chat} className={styles.start_chat} />
+            <img src={start_chat} className={styles.start_chat} alt='img' />
         </div >
     );
 };
